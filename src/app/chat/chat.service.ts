11e0e2a6620e6acd750b71c96daf759860d7c576
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-02-25 16:09:14 +0530

    resolve apiAiClient error

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {ApiAiClient} from 'api-ai-javascript/es6/ApiAiClient';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+
+  readonly token =  environment.dialogflow.angularBot;
+  readonly client = new ApiAiClient({accessToken: this.token});
 
   constructor() { }
 }
