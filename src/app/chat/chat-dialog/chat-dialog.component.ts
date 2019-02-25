abfa5f1a00c4b70df6a2547df817b4cd582c105f
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-02-25 17:01:45 +0530

    converse and update

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ChatService} from '../chat.service';
 
 @Component({
   selector: 'chat-dialog',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private chat: ChatService) { }
 
   ngOnInit() {
+    this.chat.talk();
   }
 
 }
